6cb6e774d69b383b0513078a20dc1f2a78f77d94
Rafael Cunha de Almeida <rafael@kontesti.me>
2014-04-20 16:55:31 -0300

    A very crude version of the setitem feature

--- src/dsdb.py
+++ src/dsdb.py
@@ -1,4 +1,19 @@
 import os
+import time
+import pickle
+import fcntl
+import collections
+from contextlib import contextmanager
+
+
+@contextmanager
+def lock_file(*args, **kwargs):
+    with open(*args, **kwargs) as lock:
+        fcntl.flock(lock.fileno(), fcntl.LOCK_EX)
+        # We need to open the file again in case it was modified while we were
+        # waiting for the lock
+        with open(*args, **kwargs) as f:
+            yield f
 
 
 class DeadSimple(object):
@@ -13,8 +28,27 @@ class DeadSimple(object):
         Keyword arguments:
         directory -- directory to save files into"""
         self.directory = directory
-        self.keys = {}
 
     def __setitem__(self, key, value):
+        """Synchronously values to disk under the specified key. It will always
+        change the contents of disk, regardless of whether this object was the
+        last one to modify the key or not."""
         if '/' in key:
             raise ValueError('Key cannot contain /')
+        file_name = os.path.join(self.directory, key)
+        with lock_file(file_name, 'a+b') as f:
+            f.seek(0)
+            data = f.read()
+            if data:
+                obj = pickle.loads(data)
+            else:
+                obj = {}
+            obj['version'] = obj.get('version', -1) + 1
+            obj['content'] = value
+            obj['timestamp'] = int(round(time.time()))
+            f.seek(0)
+            f.truncate()
+            f.write(pickle.dumps(obj))
+
+    def __getitem__(self, key):
+        pass
--- tests/test_inserts.py
+++ tests/test_inserts.py
@@ -1,4 +1,5 @@
 import unittest
+import pickle
 import shutil
 import os
 from dsdb import DeadSimple
@@ -31,36 +32,41 @@ class TestInsertFunction(unittest.TestCase):
             obj = pickle.loads(f.read())
             self.assertEqual(['hello'], obj.get('content'))
 
-    def test_version_increment(self):
+    def test_internal_data_handling(self):
         db = DeadSimple('tests/db')
         db['prego'] = ['hello']
         with open('tests/db/prego', 'rb') as f:
             obj = pickle.loads(f.read())
             self.assertEqual(0, obj.get('version'))
+            self.assertEqual(['hello'], obj.get('content'))
 
         db['prego'] = 'hi'
         with open('tests/db/prego', 'rb') as f:
             obj = pickle.loads(f.read())
             self.assertEqual(1, obj.get('version'))
+            self.assertEqual('hi', obj.get('content'))
 
-    def test_version_increment_with_multiple_dbs(self):
+    def test_internal_data_handling_multiple_dbs(self):
         db1 = DeadSimple('tests/db')
         db1['prego'] = ['hello']
         with open('tests/db/prego', 'rb') as f:
             obj = pickle.loads(f.read())
             self.assertEqual(0, obj.get('version'))
+            self.assertEqual(['hello'], obj.get('content'))
 
         db2 = DeadSimple('tests/db')
         db2['prego'] = 'hi'
         with open('tests/db/prego', 'rb') as f:
             obj = pickle.loads(f.read())
             self.assertEqual(1, obj.get('version'))
+            self.assertEqual('hi', obj.get('content'))
 
         db1 = DeadSimple('tests/db')
-        db1['prego'] = ['hello']
+        db1['prego'] = ['howdy']
         with open('tests/db/prego', 'rb') as f:
             obj = pickle.loads(f.read())
-            self.assertEqual(3, obj.get('version'))
+            self.assertEqual(2, obj.get('version'))
+            self.assertEqual(['howdy'], obj.get('content'))
 
     def test_slash_not_allowed_in_key(self):
         db = DeadSimple('tests/db')
