e7fc47ec0f9cb3776fa5a0f8b2d82eccf74ed6d3
Rafael Cunha de Almeida <rafael@kontesti.me>
2014-04-20 23:04:57 -0300

    more descriptions

--- setup.py
+++ setup.py
@@ -3,8 +3,11 @@ from setuptools import setup
 setup(name='dsdb',
     version='0.1',
     description='A simple database for simple needs',
+    long_description="""This is a filesystem backed schemaless database. It is very simple, but handles concurrent access.""",
     author='Rafael Cunha de Almeida',
     author_email='rafael@kontesti.me',
     package_dir = {'': 'src'},
     py_modules=['dsdb'],
+    license='MIT',
+    url='http://github.com/aflag/dsdb',
 ) 
