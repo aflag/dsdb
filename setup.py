fa198b8a13788f9a58b97710eff8e60e8c930859
Rafael Cunha de Almeida <rafael@kontesti.me>
2014-04-20 21:09:08 -0300

    setup file

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='dsdb',
+    version='0.1',
+    description='A simple database for simple needs',
+    author='Rafael Cunha de Almeida',
+    author_email='rafael@kontesti.me',
+    package_dir = {'': 'src'},
+    py_modules=['dsdb'],
+) 
